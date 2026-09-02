f7fb46fc0508e606f70563f0e644c74f6a448c70
Rohan M <rohan110620@gmail.com>
2026-09-02 21:53:30 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,6 @@ import RecommendationHistory from "./components/User/RecommendationHistory";
 import ManageFarms from "./components/ManageFarms.jsx";
 import CropScannerPage from "./pages/CropScannerPage.jsx";
 import { registerSW } from "virtual:pwa-register";
-import PwaInstallPrompt from "./components/PwaInstallPrompt.jsx";
 
 registerSW({
     immediate: true,
@@ -132,7 +131,6 @@ createRoot(document.getElementById("root")).render(
           {" "}
             <ModalProvider>
               <GlobalModal />
-              <PwaInstallPrompt />
               <RouterProvider router={router} />
           </ModalProvider>
           </LocationProvider>
