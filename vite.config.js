9fba3fcb58a620252d50f8dad38b5cd5da52d7ee
Rohan M <rohan110620@gmail.com>
2026-09-03 10:40:04 +0530

    pwa logo

--- vite.config.js
+++ vite.config.js
@@ -39,17 +39,19 @@ export default defineConfig({
             src: "/launchericon-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose:"any"
           },
           {
             src: "/launchericon-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose:"any"
           },
           {
             src: "/launchericon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
           },
         ],
       },
