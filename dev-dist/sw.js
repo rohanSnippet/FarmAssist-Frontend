2a2007a65b5cde37b8630993d60983a8e1f48062
Rohan M <rohan110620@gmail.com>
2026-09-05 10:17:39 +0530

    feat: implement offline-first crop scanning and background sync using IndexedDB

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.dk0bke88uf"
+    "revision": "0.d56vlj6cr78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
