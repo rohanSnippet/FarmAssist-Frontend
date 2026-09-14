6bd90b871ebc0bf3143ea5d5a3badd47ecc90faa
Rohan M <rohan110620@gmail.com>
2026-09-15 00:26:12 +0530

    feat: add service worker for caching and navigation routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.h0enlaqaago"
+    "revision": "0.1g1ugee9ceo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
